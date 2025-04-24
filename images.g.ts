ff155029a356fecfdaafa0f20b5ea4f74b2a85cf
raimonizard <97727989+raimonizard@users.noreply.github.com>
2025-04-24 15:29:23 +0200

    chore: maintenance

--- images.g.ts
+++ images.g.ts
@@ -1,4 +1,4 @@
-// Código generado automáticamente. No editar.
+// Auto-generated code. Do not edit.
 namespace myImages {
 
     helpers._registerFactory("image", function(name: string) {
@@ -366,4 +366,4 @@ f f f c 3 c c c c 3 c f f f
     })
 
 }
-// Código generado automáticamente. No editar.
+// Auto-generated code. Do not edit.
